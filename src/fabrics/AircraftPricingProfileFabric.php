8b8de2db7dcf8409a312f15a9e72ff0cdca381ff
xRubin <dalamar84@mail.ru>
2022-06-23 19:38:34 +0300

    urgent

--- src/fabrics/AircraftPricingProfileFabric.php
+++ src/fabrics/AircraftPricingProfileFabric.php
@@ -4,6 +4,7 @@ namespace ddd\pricing\fabrics;
 
 use ddd\pricing\entities;
 use ddd\pricing\values;
+use yii\helpers\ArrayHelper;
 
 final class AircraftPricingProfileFabric
 {
@@ -27,9 +28,12 @@ final class AircraftPricingProfileFabric
     {
         return entities\AircraftPricingProfile::load(
             new values\AircraftPricingProfileID($data->id),
-            new values\AircraftPricingProfileProperties(
+            (new values\AircraftPricingProfileProperties(
                 new values\AircraftPricingProfileName($data->name)
-            )
+            ))
+                ->setUrgent1(ArrayHelper::getValue($data, 'settings.urgent1', 0))
+                ->setUrgent2(ArrayHelper::getValue($data, 'settings.urgent2', 0))
+                ->setUrgent3(ArrayHelper::getValue($data, 'settings.urgent3', 0))
         );
     }
 }
--- src/values/AircraftPricingProfileProperties.php
+++ src/values/AircraftPricingProfileProperties.php
@@ -6,6 +6,10 @@ final class AircraftPricingProfileProperties
 {
     private AircraftPricingProfileName $name;
 
+    private int $urgent1 = 0;
+    private int $urgent2 = 0;
+    private int $urgent3 = 0;
+
     public function __construct(AircraftPricingProfileName $name)
     {
         $this->name = $name;
@@ -17,5 +21,59 @@ final class AircraftPricingProfileProperties
     public function getName(): AircraftPricingProfileName
     {
         return $this->name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getUrgent1(): int
+    {
+        return $this->urgent1;
+    }
+
+    /**
+     * @param int $urgent1
+     * @return AircraftPricingProfileProperties
+     */
+    public function setUrgent1(int $urgent1): AircraftPricingProfileProperties
+    {
+        $this->urgent1 = $urgent1;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getUrgent2(): int
+    {
+        return $this->urgent2;
+    }
+
+    /**
+     * @param int $urgent2
+     * @return AircraftPricingProfileProperties
+     */
+    public function setUrgent2(int $urgent2): AircraftPricingProfileProperties
+    {
+        $this->urgent2 = $urgent2;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getUrgent3(): int
+    {
+        return $this->urgent3;
+    }
+
+    /**
+     * @param int $urgent3
+     * @return AircraftPricingProfileProperties
+     */
+    public function setUrgent3(int $urgent3): AircraftPricingProfileProperties
+    {
+        $this->urgent3 = $urgent3;
+        return $this;
     }
 }
